2876c17966acbb7bc9b44ce3ce586a8f1f289f34
Nathan Wallis <natewallis@gmail.com>
2017-01-23 13:39:19 +1100

    getting ready to add SEO

---
+++ classes/node.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Knowledgebase;
+
+class Node{
+
+    private $id;
+    private $children;
+    private $parent;
+    private $child;
+
+    function __construct($id = ''){
+        $this->id = $id;
+    }
+
+    function getHREF(){
+        return (isset($this->parent) ? $this->parent->getHREF() . "$this->id/" : ""); 
+    }
+
+    function getContentPath(){
+        return (isset($this->parent) ? $this->parent->getContentPath() . $this->id . ".children/": "./$this->id/"); 
+    }
+
+    function toHTML(){
+
+        $returnHTML = <<<HTML
+            <div class="kb-section">
+HTML;
+
+        if ( file_exists($this->getContentPath()) || !isset($this->parent) ){
+            $this->children = glob($this->getContentPath() . "*.node");
+
+            foreach ($this->children as $child){
+
+                $childFileContents = json_decode(file_get_contents($child));
+                $childTitle = $childFileContents->title;
+                $childNodeId = basename($child, '.node');
+
+                $selectedClass = ($this->child && $childNodeId == $this->child->id) ? "kb-selected" : "";
+
+                $returnHTML .= <<<HTML
+                <a href="/{$this->getHREF()}{$childNodeId}">
+                    <div class="{$selectedClass}">{$childTitle}</div>
+                </a>
+HTML;
+            }
+
+        }else{
+            $childFileContents = json_decode(file_get_contents($this->parent->getContentPath() . "$this->id.node"));
+            $returnHTML .= <<<HTML
+                <div>{$childFileContents->text}</div>
+HTML;
+        }
+
+        $fieldsDescriptionPath = $this->getContentPath() . "node.fields";
+
+        if (file_exists($fieldsDescriptionPath)){
+            $fieldDescriptors = json_decode(file_get_contents($fieldsDescriptionPath));
+
+            //Generate form html
+            $returnHTML .= '<form action="" method = "post">';
+
+            foreach ($fieldDescriptors->fields as $descriptor){
+
+                $inputClass = isset($fieldDescriptors->seo_translate_key) && ($fieldDescriptors->seo_translate_key == $descriptor->key_name) ? "kb-seo-translate" : "";
+
+                $returnHTML .= <<<HTML
+                    <label>{$descriptor->name}<input class="{$inputClass}" type="text" name="fields[{$descriptor->key_name}]"></label>
+HTML;
+            }
+
+            $encodedContentPath = htmlspecialchars(base64_encode($this->getContentPath()));
+
+            $returnHTML .= <<<HTML
+                <input type="hidden" name="parent_node" value="{$encodedContentPath}">
+                <input type="submit" value="Save">
+                </form> 
+HTML;
+        }
+
+        $returnHTML .= <<<HTML
+
+            <script>
+                $(".kb-seo-translate").keyup(function(){
+                    console.log($(this).val());    
+                });
+            </script>
+ 
+            </div>
+HTML;
+
+        return (isset($this->parent) ? $this->parent->toHTML() : '') . $returnHTML;
+
+    }
+
+    function getParent(){
+        return $parent;
+    }
+
+    function getChild(){
+        return $child;
+    }
+
+    function assignParent($parent){
+        $this->parent = $parent;
+    }
+
+    function assignChild($child){
+        $this->child = $child;
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,83 +1,44 @@
 <?php
 
-namespace Knowledgebase;
+include_once('./classes/node.php');
+
+use Knowledgebase\Node;
 
 const CONTENT_DIRECTORY = "content";
 
-class Node{
-
-    private $id;
-    private $children;
-    private $parent;
-    private $child;
-
-    function __construct($id = ''){
-        $this->id = $id;
+function GUID()
+{
+    if (function_exists('com_create_guid') === true)
+    {
+        return trim(com_create_guid(), '{}');
     }
 
-    function getHREF(){
-        return (isset($this->parent) ? $this->parent->getHREF() . "$this->id/" : ""); 
+    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+}
+
+if (isset($_REQUEST['parent_node'])){
+
+    //Generate a new guid for node name
+    $guid = GUID();
+    $parentNode = base64_decode($_REQUEST['parent_node']);
+    $nodeFile = "$parentNode/$guid.node";
+
+    $fieldsDescriptionPath = "$parentNode/node.fields";
+    $childDescriptionDirectory = "$parentNode$guid.children/";
+    $fieldDescriptors = json_decode(file_get_contents($fieldsDescriptionPath));
+
+    //Build JSON to save in new node
+    $newNodeJSON = json_encode($_REQUEST['fields']);
+
+    //Save the node under the parent node
+    file_put_contents($nodeFile, $newNodeJSON);
+
+    //Create child fields
+    if (isset($fieldDescriptors->childFields)){
+        mkdir($childDescriptionDirectory, 0777);
+        file_put_contents($childDescriptionDirectory . "node.fields", json_encode($fieldDescriptors->childFields));
     }
 
-    function getContentPath(){
-        return (isset($this->parent) ? $this->parent->getContentPath() . $this->id . ".children/": "./$this->id/"); 
-    }
-
-    function toHTML(){
-        
-        $returnHTML = <<<HTML
-            <div class="kb-section">
-HTML;
-
-        if ( file_exists($this->getContentPath()) || !isset($this->parent) ){
-            $this->children = glob($this->getContentPath() . "*.node");
-
-            foreach ($this->children as $child){
-
-                $childFileContents = json_decode(file_get_contents($child));
-                $childTitle = $childFileContents->title;
-                $childNodeId = basename($child, '.node');
-
-                $selectedClass = ($this->child && $childNodeId == $this->child->id) ? "kb-selected" : "";
-
-                $returnHTML .= <<<HTML
-                <a href="/{$this->getHREF()}{$childNodeId}">
-                    <div class="{$selectedClass}">{$childTitle}</div>
-                </a>
-HTML;
-            }
-
-        }else{
-            $childFileContents = json_decode(file_get_contents($this->parent->getContentPath() . "$this->id.node"));
-            $returnHTML .= <<<HTML
-                <div>{$childFileContents->text}</div>
-HTML;
-        }
-
-
-        $returnHTML .= <<<HTML
-            </div>
-HTML;
-
-        return (isset($this->parent) ? $this->parent->toHTML() : '') . $returnHTML;
-
-    }
-
-    function getParent(){
-        return $parent;
-    }
-
-    function getChild(){
-        return $child;
-    }
-
-    function assignParent($parent){
-        $this->parent = $parent;
-    }
-
-    function assignChild($child){
-        $this->child = $child;
-    }
 }
 
 //Server URI needs some massaging
@@ -89,13 +50,14 @@ foreach ($nodePaths as $path){
 
     $childNode = new Node($path);
 
-    if (isset($tempRoot)){
-        $tempRoot->assignChild($childNode);
-        $childNode->assignParent($tempRoot);
+    if (isset($root)){
+        $root->assignChild($childNode);
+        $childNode->assignParent($root);
     }
 
-    $tempRoot = $childNode;
+    $root = $childNode;
 }
+
 ?>
 
 
@@ -104,11 +66,15 @@ foreach ($nodePaths as $path){
 <head>
     <meta charset="UTF-8">
     <title>Bar Fridges Australia Knowledgebase</title>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="./js/kb.js"></script>
 </head>
 <body>
 <?php 
 
-echo $tempRoot->toHTML(); 
+
+
+echo $root->toHTML(); 
 
 ?>
 
