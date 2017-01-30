3db9a84b8bff4d51783aea0b6244aa7445f227dd
Nathan Wallis <natewallis@gmail.com>
2017-01-30 13:23:00 +1100

    New javascript libraries for dialog and image zoom

--- js/kb.js
+++ js/kb.js
@@ -5,16 +5,31 @@ $(function() {
         $(this).parent().siblings('.seo-name').val(seoName);
     });
 
+    $(".settings-dialog").dialog({
+        autoOpen: false,
+        modal:true,
+        title:"Add new",
+        width:800
+    });
+
+    $(".add-node").click(function(){
+        var dialog = $(this).attr('for');
+        $("#" + dialog).dialog("open");
+    });
+
+    $(".fridge-picture").elevateZoom();
+
 });
 
-function convertToSEO(inputString){
+function convertToSEO(inputString) {
 
-  var encodedUrl = inputString.toString().toLowerCase(); 
-  encodedUrl = encodedUrl.trim();
-  encodedUrl = encodedUrl.split(/\&+/).join("-and-")
-  encodedUrl = encodedUrl.split(/[^a-z0-9]/).join("-");       
-  encodedUrl = encodedUrl.split(/-+/).join("-");
-  encodedUrl = encodedUrl.trim(); 
-  return encodedUrl; 
+    var encodedUrl = inputString.toString().toLowerCase();
+    encodedUrl = encodedUrl.trim();
+    encodedUrl = encodedUrl.replace(/\?/, "");
+    encodedUrl = encodedUrl.split(/\&+/).join("-and-")
+    encodedUrl = encodedUrl.split(/[^a-z0-9]/).join("-");
+    encodedUrl = encodedUrl.split(/-+/).join("-");
+    encodedUrl = encodedUrl.trim();
+    return encodedUrl;
 
 }
